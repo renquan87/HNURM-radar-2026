972e78c4c4acef8709278096de601564b3676252
xuxin <lisian_magic@163.com>
2024-11-14 03:34:07 +0800

    add pubish_video

---
+++ src/hnurm_radar/hnurm_radar/publish_video.py
@@ -0,0 +1,34 @@
+import rclpy
+from rclpy.node import Node
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge
+import cv2
+
+class VideoPublisher(Node):
+    def __init__(self):
+        super().__init__('video_publisher')
+        self.declare_parameter('video_file', 'path/to/video.mp4')
+        video_file = self.get_parameter('video_file').get_parameter_value().string_value
+        self.get_logger().info('video_file: %s' % video_file)
+        self.publisher_ = self.create_publisher(Image, 'camera_raw', 10)
+        self.timer = self.create_timer(0.014, self.timer_callback)
+        self.cap = cv2.VideoCapture(video_file)
+        self.bridge = CvBridge()
+        self.get_logger().info('Publishing video from: %s' % video_file)
+    def timer_callback(self):
+        ret, frame = self.cap.read()
+        if ret:
+            msg = self.bridge.cv2_to_imgmsg(frame, encoding='bgr8')
+            self.publisher_.publish(msg)
+        else:
+            self.cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
+
+def main(args=None):
+    rclpy.init(args=args)
+    video_publisher = VideoPublisher()
+    rclpy.spin(video_publisher)
+    video_publisher.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
