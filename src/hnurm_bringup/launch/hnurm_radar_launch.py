c4305134592e6c9f463a939e066ea31b8eadc6af
Lisian <lisian_magic@163.com>
2025-03-11 17:01:54 +0800

    修复launch文件报错

--- src/hnurm_bringup/launch/hnurm_radar_launch.py
+++ src/hnurm_bringup/launch/hnurm_radar_launch.py
@@ -6,14 +6,9 @@ from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 
 def generate_launch_description():
-    camera_dir = get_package_share_directory('hnurm_camera')
-    params_file = LaunchConfiguration('params_file')
+
+
     return LaunchDescription([
-        DeclareLaunchArgument(
-            'params_file',
-            default_value=os.path.join(camera_dir, 'params', 'default.yaml'),
-            description='Full path to the ROS2 parameters file to use for the camera'
-        ),
         Node(
             package='hnurm_radar',
             executable='lidar_node',
